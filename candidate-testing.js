7ffdacbd9774d99794be750868c6414a34c6a8e3
apdias <>
2021-06-06 20:11:32 +0000

    Part 2 completed.

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,39 +1,48 @@
 const input = require('readline-sync');
 
+
 // TODO 2: modify your quiz app to ask 5 questions //
+
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = " ";
 
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
+let question = " ";
+let correctAnswer = " ";
 let candidateAnswer = " ";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let candidateAnswers = [];
+
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? " ];
+
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Enter you name: ");
+candidateName = input.question("Candidate Name: ");
 }
 
+
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question("Who was the first American woman in space? ");
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer. Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user's responses. //
+
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswers[i] = input.question(`${i+1}) ${questions[i]}`);
+  console.log(`Your Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}`);
+  console.log();
+}
+
 }
 
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
-if (candidateAnswer === "Sally Ride") {
-  console.log("Well done, correct answer! ");
-} else {
-  console.log("Incorrect, try again! ");
-}
+
+
 
 
   let grade;
@@ -42,10 +51,13 @@ if (candidateAnswer === "Sally Ride") {
   return grade;
 }
 
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
 console.log("Hello, " + candidateName + "!");
+console.log(" ");
+
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
